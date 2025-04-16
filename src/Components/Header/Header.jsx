3fcfc4a314071db2037a03e5313c9c3fa16fbc59
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2025-04-16 14:56:45 +0200

    ajout du header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return <nav className='header'>
+    <div className='header_logo'>
+    <img src="/img/logo.png" alt="logo" />
+    </div>
+    <div className='acceuil_and_a_propos'>
+        <div className='accueil'>Accueil</div>
+        <div className='a_propos'>A propos</div> 
+    </div>
+   
+  </nav>;
+}
+
+export default Header;
