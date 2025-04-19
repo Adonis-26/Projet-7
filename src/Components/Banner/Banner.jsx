7016544bd0f06529c5d9f09ca16367038da7cd5c
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2025-04-19 14:01:04 +0200

    banner ajustement

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import './Banner.scss';
 function Banner() {
   return <div className='banner'>
 
-    <h1>Chez vous, partout et ailleurs</h1>
+    <h3>Chez vous, partout et ailleurs</h3>
   </div>
   
 }
