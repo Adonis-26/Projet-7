88f7377fd89c0fb620f38da8f8f1faff876e732a
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2025-04-19 13:21:07 +0200

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
