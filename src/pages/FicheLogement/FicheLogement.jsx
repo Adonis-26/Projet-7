ff2e48dccfd1e4a15fb5429858ace50746ce287a
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2025-05-20 18:38:29 +0200

    ajout dynaminque du reste des informations. il ne reste que le scss à mettre à jour

--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -8,7 +8,7 @@ import data from '../../../public/data.json';
 function FicheLogement() {
   const { apartementId } = useParams(); // On récupère l'id de l'appartement depuis l'URL
   const apartement = data.find((apt) => apt.id === apartementId); // On cherche dans le fichier JSON
-console.log("apartement:", apartement);
+  console.log("apartement:", apartement);
 
   if (!apartement) {
     return <p>Appartement non trouvé.</p>; // Affiche un message si l'appartement n'existe pas
@@ -16,34 +16,36 @@ console.log("apartement:", apartement);
 
   return (
     <div className='fichelogement'>
-      <BannerAppartement imageUrl={apartementId.cover} />
-
+      <div className='BannerAppartement'>
+          <img className='tel' src={apartement.cover} alt={apartement.cover} />
+      </div>
       <div className='responsive'>
-      <div className='ville_flex'>
-        <div className='ville'>
-          <p className='classTitle'>{apartement.title}</p>
-          <p className='region'>{apartement.location}</p>
+        <div className='ville_flex'>
+          <div className='ville'>
+            <p className='classTitle'>{apartement.title}</p>
+            <p className='region'>{apartement.location}</p>
             <div className='bloc-rectangle' >
-              {apartement.tags.map((item, index) =>(
+              {apartement.tags.map((item, index) => (
                 <p className='rectangle' key={index}>{item}</p>
               ))}
-              
+            </div>
+          </div>
         </div>
-    </div>
-  </div>
-        <div className='AlexAndStars'>
-       <div className='stars'>
-            <i className="fa-solid fa-star"></i>
-            <i className="fa-solid fa-star"></i>
-            <i className="fa-solid fa-star"></i>
-            <i className="fa-solid gray fa-star"></i>
-            <i className="fa-solid gray fa-star"></i>
-        </div> 
-        <div className='user'>
-           <p>Alexandre <br/>Dumas</p> 
-            <span className='cercle'></span>
+        <div className='containerUserStars'>
+          <div className='stars'>
+            {[...Array(5)].map((_, index) => (
+              <i
+                key={index}
+                className={`fa-star ${index < parseInt(apartement.rating) ? "fa-solid" : "fa-regular"}`}
+              ></i>
+            ))}
+          </div>
+          <div className='user'>
+            <p className='rectangle'>{apartement.host.name}</p>
+            <img className='cercle' src={apartement.host.picture} alt={apartement.host.name} />
+          </div>
+
         </div>
-    </div>
       </div>
       <div className='collapse_flex'>
         <Collapse title="Description">{apartement.description}</Collapse>
